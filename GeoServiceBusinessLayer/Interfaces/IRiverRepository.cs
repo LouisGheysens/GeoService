98418461960f8b1e91ccaef0032acbcdeca7ea44
Louis_Gheysens <71761136+LouisGheysens@users.noreply.github.com>
2021-11-12 20:08:19 +0100

    BusinessLaag klassen ADVH klassendiagram

---
+++ GeoServiceBusinessLayer/Interfaces/IRiverRepository.cs
@@ -0,0 +1,17 @@
+﻿using GeoServiceBusinessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoServiceBusinessLayer.Interfaces {
+    public interface IRiverRepository {
+        void AddRiver(River river, int countryId);
+        River GetRiver(int id);
+        IEnumerable<River> GetAll();
+        void RemoveRiver(int riverId);
+        void UpdateRiver(River river);
+        bool ExistsRiver(int id);
+    }
+}
